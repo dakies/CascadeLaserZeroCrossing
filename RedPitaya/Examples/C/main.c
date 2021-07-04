bdcc056937281ab53e1437122a6581dceef8ed68
Daniel <daniekie@studetn.ethz.ch>
2021-07-05 00:25:52 +0200

    Added define

--- RedPitaya/Examples/C/main.c
+++ RedPitaya/Examples/C/main.c
@@ -6,48 +6,59 @@
 
 #include "rp.h"
 
+#define TS 0.000004
 
-void predict_state(double *state, const double ts) {
+// delete fprint
+// block vs smaple by sample
+// double/float
+// scale freq in terms of ts
+// variable times variable vs cos cos
+// 2*ts constant
+// define ts, so its not passed as parameter (or static variable)
+// Thermal throttilng
+// DVFS
+
+void predict_state(float *state, const float ts) {
     state[0] = state[0] + state[1] * ts;
 }
 
-void predict_P(double *P, const double *Q, const double ts) {
+void predict_P(float *P, const float *Q, const float ts) {
     P[0] = P[0] + 2 * ts * P[3] + ts * ts * P[1] + Q[0];
     P[1] = P[1] + Q[1];
     P[2] = P[2] + Q[2];
     P[3] = P[3] + ts * P[1] + Q[3];
 }
 
-double alpha(double const *state, double const *P) {
-    return P[0] * state[0] * state[0] * cos(state[0])*cos(state[0]) + P[2] * sin(state[0])*sin(state[0]);
+float alpha(float const *state, float const *P) {
+    return P[0] * state[0] * state[0] * cosf(state[0])*cosf(state[0]) + P[2] * sinf(state[0])*sinf(state[0]);
 }
 
-double calc_S(const double *state, const double *P, const double R) {
+float calc_S(const float *state, const float *P, const float R) {
 
     return alpha(state, P) + R;
 }
 
-void gain(double *K, const double *state, const double *P, double r) {
-    double s = calc_S(state, P, r);
-    K[0] = P[0] * state[2] * cos(state[0]);
+void gain(float *K, const float *state, const float *P, float r) {
+    float s = calc_S(state, P, r);
+    K[0] = P[0] * state[2] * cosf(state[0]);
     K[0] = s;
-    K[1] = P[3] * state[2] * cos(state[0]);
+    K[1] = P[3] * state[2] * cosf(state[0]);
     K[1] /= s;
-    K[2] = P[2] * sin(state[0]);
+    K[2] = P[2] * sinf(state[0]);
     K[2] /= s;
 }
 
-void update_state(double *state, double *K, double *P, double measurement,  double r) {
-    double y = measurement - state[2] * sin(state[0]);
-    double k[3];
+void update_state(float *state, float *P, float measurement,  float r) {
+    float y = measurement - state[2] * sinf(state[0]);
+    float k[3];
     gain(k, state, P, r);
     state[0] = state[0] + y * k[0];
     state[1] = state[1] + y * k[1];
     state[2] = state[2] + y * k[2];
 }
 
-void update_p(double *P, double *state, double r) {
-    double a = alpha(state, P);
+void update_p(float *P, float *state, float r) {
+    float a = alpha(state, P);
     a = (1 - a / (a + r));
     P[0] *= a;
     P[1] *= a;
@@ -56,17 +67,16 @@ void update_p(double *P, double *state, double r) {
 }
 
 int main() {
-    const double ts = 0.001;
     _Bool half_phase;
-    //double phase_prev;
-    double x[3];
-    double p[4];
-    double q[3] = {1,1,1};
-    double k[3];
-    double z;
-    double r = 0.0001;
-    //double s;
-    //double a;
+    //float phase_prev;
+    float x[3];
+    float p[4];
+    float q[3] = {1,1,1};
+    //float k[3];
+    float z;
+    float r = 0.0001;
+    //float s;
+    //float a;
 
     /* Print error, if rp_Init() function failed */
     if (rp_Init() != RP_OK) {
@@ -76,7 +86,7 @@ int main() {
     // fp = fopen("/time_log.txt", "w");
 
 
-    uint32_t buff_size = 16384;
+    uint32_t buff_size = 1;
     float *buff1 = (float *) malloc(buff_size * sizeof(float));
     float *buff2 = (float *) malloc(buff_size * sizeof(float));
 
@@ -85,16 +95,6 @@ int main() {
     rp_AcqSetTriggerDelay(0);
 
     rp_AcqStart();
-
-    /* After acquisition is started some time delay is needed in order to acquire fresh samples in to buffer*/
-    /* Here we have used time delay of one second but you can calculate exact value taking in to account buffer*/
-    /*length and smaling rate*/
-    sleep(1);
-    //fscanf
-    rp_AcqSetTriggerSrc(RP_TRIG_SRC_NOW);
-    // rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
-
-
     //while (1)
     for(int i=0; i<10000 ;++i){
         // Time for iteration of filter
@@ -103,12 +103,15 @@ int main() {
         // Get data from buffer
         rp_AcqGetOldestDataV(RP_CH_1, &buff_size, buff1);
         //rp_AcqGetOldestDataV(RP_CH_2, &buff_size, buff2);
+
+        // If buffer empty, skip...
         z=buff1[0];
 
-        predict_state(x, ts);
-        predict_P(p, q, ts);
-        update_state(x, k, p, z, r);
+        predict_state(x, TS);
+        predict_P(p, q, TS);
+        update_state(x, p, z, r);
         update_p(p, x, r);
+
         if(x[0]>2*M_PI){
             x[0] = x[0] - 2*M_PI;
         }
@@ -121,14 +124,14 @@ int main() {
 
         // End timing
         clock_t end = clock();
-        double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
+        float time_spent = (float) (end - begin); // / CLOCKS_PER_SEC;
 
         printf("%d", half_phase);
 
         //Save time
-        fprintf(stderr, "%lf", time_spent);
+        fprintf(stderr, "%lf\n", time_spent);
     }
-    /* Releasing resources */
+    /* Releasinfg resources */
     free(buff1);
     free(buff2);
     rp_Release();
